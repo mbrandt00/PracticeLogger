62ce173e999d39fdf93dc2f037dc85146ef3f07b
Michael brandt <msbrandt00@gmail.com>
2024-02-23 19:44:38 -0700

    Initial Commit

---
+++ PracticeLogger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PracticeLogger
+//
+//  Created by Michael Brandt on 2/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeLogger/PracticeLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeLoggerApp.swift
+//  PracticeLogger
+//
+//  Created by Michael Brandt on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
