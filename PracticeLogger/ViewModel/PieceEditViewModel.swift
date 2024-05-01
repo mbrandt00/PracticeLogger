81962cc640b682d1c8b2b43f4a76ead05a913fac
Michael Brandt <michael.brandt@rockies.com>
2024-04-30 21:59:53 -0600

    Add movement creation in supabase

--- PracticeLogger/ViewModel/PieceEditViewModel.swift
+++ PracticeLogger/ViewModel/PieceEditViewModel.swift
@@ -12,17 +12,47 @@ class PieceEditViewModel: ObservableObject {
         do {
             let currentUserID = try await Database.shared.getCurrentUser().id
 
-            let query = try Database.shared.client.from("pieces").insert([
-                "work_name": piece.workName,
-                "user_id": currentUserID.uuidString
-            ])
+            let dbPiece: DbPiece = try await Database.shared.client
+                .from("pieces")
+                .insert([
+                    "work_name": piece.workName,
+                    "user_id": currentUserID.uuidString
+                ])
+                .select()
+                .single()
+                .execute()
+                .value
 
-            // Execute the insert query asynchronously
-            let response = try await query.execute()
-            print(response)
+            let newPieceId = dbPiece.id
+            print("newPieceId", newPieceId)
+
+            for movement in piece.movements {
+                print(movement)
+                var dbMovement: DbPieceMovement = try await Database.shared.client
+                    .from("movements")
+                    .insert([
+                        "movement_name": movement.name,
+                        "movement_number": String(movement.number),
+                        "piece_id": String(newPieceId)
+                    ]).select().single().execute().value
+            }
+
         } catch {
             // Handle any errors
             throw error
         }
     }
+}
+
+struct DbPiece: Codable {
+  let id: Int
+  let work_name: String
+  let user_id: UUID
+}
+
+struct DbPieceMovement: Codable {
+  let id: Int
+  let movement_name: String
+  let movement_number: Int
+    let piece_id: Int
 }
