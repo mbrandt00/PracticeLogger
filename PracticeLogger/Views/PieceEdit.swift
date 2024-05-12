14154ce3e018157781e587f839f2c885e1d6af48
Michael Brandt <michael.brandt@rockies.com>
2024-05-12 10:58:07 -0600

    Editing work name WIP

--- PracticeLogger/Views/PieceEdit.swift
+++ PracticeLogger/Views/PieceEdit.swift
@@ -8,73 +8,76 @@
 import SwiftUI
 import AlertToast
 struct PieceEdit: View {
-    let piece: Piece
+    var piece: Piece
     @ObservedObject var viewModel = PieceEditViewModel()
     @State private var showToast: Bool = false
     @State private var errorMessage: String = ""
 
     var body: some View {
         VStack {
-            VStack(alignment: .leading, spacing: 10) {
-                VStack(alignment: .leading, spacing: 10) {
             Text(piece.workName)
                 .font(.title)
-            ForEach(piece.movements) { movement in
-                HStack {
-                    Text(movement.number.toRomanNumeral() ?? "")
-                        .font(.caption)
-                        .frame(width: 24, height: 14)
-                        .background(Color.gray.opacity(0.2))
-                        .cornerRadius(6)
-
-                    Text(movement.name)
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                        .padding(.leading)
-
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-            }
-        }
-        .padding(.horizontal, 10)
-                // Your existing code for displaying piece details
-            }
-            .padding(.horizontal, 10)
-            VStack {
-                Button(action: {
-                    Task {
-                        do {
-                            try await viewModel.insertPiece(piece: piece)
-                        } catch {
-                            // Handle the error and show the error message in a toast
-                            if let supabaseError = error as? SupabaseError {
-                                switch supabaseError {
-                                case .pieceAlreadyExists:
-                                    errorMessage = "You have already added this piece"
-                                // Add more cases as needed
+            List {
+                Section(header: Text("Movements").font(.headline).foregroundColor(.secondary)) {
+                    ForEach(piece.movements.indices, id: \.self) { index in
+                        var movement = piece.movements[index]
+                        HStack {
+                            Text(movement.number.toRomanNumeral() ?? "")
+                                .font(.caption)
+                                .frame(width: 24, height: 14)
+                                .foregroundColor(.secondary)
+                            TextField("", text: Binding(
+                                get: {
+                                    movement.name
+                                },
+                                set: { newValue, _ in // Ignore the transaction parameter
+                                    if let index = piece.movements.firstIndex(where: { $0.id == movement.id }) {
+                                        movement.name = newValue
+                                    }
                                 }
-                            } else {
-                                errorMessage = "An unexpected error occurred."
-                            }
-                            showToast = true
+                            ))
+                            .font(.subheadline)
                         }
+                        .padding(.vertical, 4)
                     }
-                }, label: {
-                    Text("Create")
-                })
-                .buttonStyle(.bordered)
-                .foregroundColor(.black)
-                .padding(3)
+                    
+                }
+                .listRowBackground(Color.clear)
+                .padding(.vertical, 3)
             }
+            .background(Color(UIColor.systemGroupedBackground))
+        
+                    Button(action: {
+                        Task {
+                            do {
+                                let dbPiece = try await viewModel.insertPiece(piece: piece)
+                                print(dbPiece)
+                            } catch {
+                                if let supabaseError = error as? SupabaseError {
+                                    switch supabaseError {
+                                    case .pieceAlreadyExists:
+                                        errorMessage = "You have already added this piece"
+                                        // Add more cases as needed
+                                    }
+                                } else {
+                                    errorMessage = "An unexpected error occurred."
+                                }
+                                showToast = true
+                            }
+                        }
+                    }, label: {
+                        Text("Create")
+                    })
+                    .buttonStyle(.bordered)
+                    .foregroundColor(.black)
+                    .padding(3)
+            Spacer()
         }
-        // Show the alert toast when showErrorToast is true
-        .toast(isPresenting: $showToast){
-
+        .toast(isPresenting: $showToast) {
             AlertToast(displayMode: .hud, type: .error(.red), title: errorMessage)
         }
     }
 }
-
 
 #Preview {
     PieceEdit(piece: Piece(workName: "Sonata 2 in B flat Minor Funeral March", composer: Composer(name: "Frederic Chopin"), movements:
