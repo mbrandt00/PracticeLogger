0c35ef2ed47822ac63f85ae220c3dd42cda210cb
Michael Brandt <msbrandt00@gmail.com>
2024-07-15 13:56:46 -0600

    Use supabase session to keep user logged in

--- PracticeLogger/ContentView.swift
+++ PracticeLogger/ContentView.swift
@@ -9,14 +9,15 @@ import SwiftUI
 import MusicKit
 
 struct ContentView: View {
-    @State var selectedTab: Tabs = .start
-    @State var isTyping: Bool = false
-    @State var isSignedIn: Bool = false
-    @State var isExpanded: Bool = false
-    @State var practiceSessionManager: PracticeSessionManager?
+    @State private var selectedTab: Tabs = .start
+    @State private var isTyping: Bool = false
+    @State private var isSignedIn: Bool = false
+    @State private var isExpanded: Bool = false
+    @State private var practiceSessionManager: PracticeSessionManager?
     @Namespace private var animation
 
     let bpm: Double = 60
+
     var body: some View {
         VStack {
             if isSignedIn {
@@ -25,42 +26,66 @@ struct ContentView: View {
                         ExpandedBottomSheet(animation: animation, activeSession: manager.activeSession!, expandedSheet: $isExpanded)
                             .transition(.asymmetric(insertion: .identity, removal: .offset(y: -5)))
                     } else {
+                        VStack {
+                            switch selectedTab {
+                            case .progress:
+                                ProgressView().environmentObject(manager)
+                            case .start:
+                                CreatePiece(isTyping: $isTyping).environmentObject(manager)
+                            case .profile:
+                                Profile(isSignedIn: $isSignedIn).environmentObject(manager)
+                            }
 
-                    VStack {
-                                switch selectedTab {
-                                case .progress:
-                                    ProgressView()
-                                case .start:
-                                    CreatePiece(isTyping: $isTyping)
-                                case .profile:
-                                    Profile(isSignedIn: $isSignedIn) .environmentObject(manager)
-                                }
-
-                        Spacer()
-                        CustomTabBar(selectedTab: $selectedTab, isTyping: $isTyping, expandedSheet: $isExpanded, animation: animation).environmentObject(manager)
-                    }
+                            Spacer()
+                            CustomTabBar(selectedTab: $selectedTab, isTyping: $isTyping, expandedSheet: $isExpanded, animation: animation).environmentObject(manager)
+                        }
                     }
                 } else {
-                    // This case is just to handle the moment right after login before the manager is set
                     ProgressView("Loading...")
                         .onAppear {
-                            practiceSessionManager = PracticeSessionManager()
+                            initializePracticeSessionManager()
                         }
                 }
             } else {
                 SignIn(isSignedIn: $isSignedIn)
                     .onChange(of: isSignedIn) { newValue in
                         if newValue {
-                            practiceSessionManager = PracticeSessionManager()
+                            initializePracticeSessionManager()
                         }
                     }
             }
         }
+        .onAppear {
+            checkSupabaseSession()
+        }
+    }
+
+    private func checkSupabaseSession() {
+        Task {
+            do {
+                _ = try await Database.getCurrentUser()
+                isSignedIn = true
+                initializePracticeSessionManager()
+            } catch {
+                isSignedIn = false
+                debugPrint(error)
+            }
+        }
+    }
+
+    private func initializePracticeSessionManager() {
+        self.practiceSessionManager = PracticeSessionManager()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
 #Preview {
     NavigationView {
-        ContentView() // .navigationBarHidden(true)
+        ContentView()
     }
 }
