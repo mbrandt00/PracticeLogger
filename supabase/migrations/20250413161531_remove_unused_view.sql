2e83da3e5f8bbb0bfc4274f785e57136b21423c6
Michael Brandt <msbrandt00@gmail.com>
2025-04-13 11:36:27 -0600

    fix auth policy

--- supabase/migrations/20250413161531_remove_unused_view.sql
+++ supabase/migrations/20250413161531_remove_unused_view.sql
@@ -1,5 +1,6 @@
 drop view public.user_unique_piece_sessions_v;
 
 ALTER TABLE collections ENABLE ROW LEVEL SECURITY;
-CREATE POLICY auth_read_collections ON practice_sessions FOR
+CREATE POLICY auth_read_collections ON collections FOR
 SELECT TO authenticated USING (true);
+
