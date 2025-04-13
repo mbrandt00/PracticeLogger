0dffbc521ad41c53c0ff040d1e4b396596fffc39
Michael Brandt <91398645+mbrandt00@users.noreply.github.com>
2025-04-12 22:48:08 -0700

    Collection UI (#29)
    * dedupe collection view in query, wip collection ui
    
    * Move collection view to sheet
    
    * Collection UI WIP

--- ApolloGQL/Sources/Operations/Queries/CollectionsQuery.graphql.swift
+++ ApolloGQL/Sources/Operations/Queries/CollectionsQuery.graphql.swift
@@ -7,7 +7,8 @@ public class CollectionsQuery: GraphQLQuery {
   public static let operationName: String = "CollectionsQuery"
   public static let operationDocument: ApolloAPI.OperationDocument = .init(
     definition: .init(
-      #"query CollectionsQuery($filter: CollectionsFilter = {  }, $orderBy: [CollectionsOrderBy!] = [], $piecesOrderBy: [PieceOrderBy!] = [{ catalogueNumber: AscNullsLast }]) { collectionsCollection(filter: $filter, orderBy: $orderBy) { __typename edges { __typename node { __typename name composer { __typename name } pieces(orderBy: $piecesOrderBy) { __typename edges { __typename node { __typename workName userId } } } } } } }"#
+      #"query CollectionsQuery($filter: CollectionsFilter = {  }, $orderBy: [CollectionsOrderBy!] = [], $piecesOrderBy: [PieceOrderBy!] = [{ catalogueNumber: AscNullsLast }]) { collectionsCollection(filter: $filter, orderBy: $orderBy) { __typename edges { __typename node { __typename name composer { __typename name } pieces(orderBy: $piecesOrderBy) { __typename edges { __typename node { __typename ...PieceDetails id workName totalPracticeTime userId } } } } } } }"#,
+      fragments: [PieceDetails.self]
     ))
 
   public var filter: GraphQLNullable<CollectionsFilter>
@@ -255,28 +256,117 @@ public class CollectionsQuery: GraphQLQuery {
                 public static var __parentType: any ApolloAPI.ParentType { ApolloGQL.Objects.Piece }
                 public static var __selections: [ApolloAPI.Selection] { [
                   .field("__typename", String.self),
+                  .field("id", ApolloGQL.BigInt.self),
                   .field("workName", String.self),
+                  .field("totalPracticeTime", Int?.self),
                   .field("userId", ApolloGQL.UUID?.self),
+                  .fragment(PieceDetails.self),
                 ] }
 
+                public var id: ApolloGQL.BigInt { __data["id"] }
                 public var workName: String { __data["workName"] }
+                public var totalPracticeTime: Int? { __data["totalPracticeTime"] }
                 public var userId: ApolloGQL.UUID? { __data["userId"] }
+                public var lastPracticed: ApolloGQL.Datetime? { __data["lastPracticed"] }
+                public var catalogueType: GraphQLEnum<ApolloGQL.CatalogueType>? { __data["catalogueType"] }
+                public var keySignature: GraphQLEnum<ApolloGQL.KeySignatureType>? { __data["keySignature"] }
+                public var format: GraphQLEnum<ApolloGQL.PieceFormat>? { __data["format"] }
+                public var instrumentation: [String?]? { __data["instrumentation"] }
+                public var wikipediaUrl: String? { __data["wikipediaUrl"] }
+                public var imslpUrl: String? { __data["imslpUrl"] }
+                public var compositionYear: Int? { __data["compositionYear"] }
+                public var catalogueNumberSecondary: Int? { __data["catalogueNumberSecondary"] }
+                public var catalogueTypeNumDesc: String? { __data["catalogueTypeNumDesc"] }
+                public var compositionYearDesc: String? { __data["compositionYearDesc"] }
+                public var compositionYearString: String? { __data["compositionYearString"] }
+                public var pieceStyle: String? { __data["pieceStyle"] }
+                public var subPieceType: String? { __data["subPieceType"] }
+                public var subPieceCount: Int? { __data["subPieceCount"] }
+                public var collectionId: ApolloGQL.BigInt? { __data["collectionId"] }
+                public var collection: Collection? { __data["collection"] }
+                public var catalogueNumber: Int? { __data["catalogueNumber"] }
+                public var nickname: String? { __data["nickname"] }
+                public var composerId: ApolloGQL.BigInt? { __data["composerId"] }
+                public var composer: Composer? { __data["composer"] }
+                public var movements: Movements? { __data["movements"] }
+
+                public struct Fragments: FragmentContainer {
+                  public let __data: DataDict
+                  public init(_dataDict: DataDict) { __data = _dataDict }
+
+                  public var pieceDetails: PieceDetails { _toFragment() }
+                }
 
                 public init(
+                  id: ApolloGQL.BigInt,
                   workName: String,
-                  userId: ApolloGQL.UUID? = nil
+                  totalPracticeTime: Int? = nil,
+                  userId: ApolloGQL.UUID? = nil,
+                  lastPracticed: ApolloGQL.Datetime? = nil,
+                  catalogueType: GraphQLEnum<ApolloGQL.CatalogueType>? = nil,
+                  keySignature: GraphQLEnum<ApolloGQL.KeySignatureType>? = nil,
+                  format: GraphQLEnum<ApolloGQL.PieceFormat>? = nil,
+                  instrumentation: [String?]? = nil,
+                  wikipediaUrl: String? = nil,
+                  imslpUrl: String? = nil,
+                  compositionYear: Int? = nil,
+                  catalogueNumberSecondary: Int? = nil,
+                  catalogueTypeNumDesc: String? = nil,
+                  compositionYearDesc: String? = nil,
+                  compositionYearString: String? = nil,
+                  pieceStyle: String? = nil,
+                  subPieceType: String? = nil,
+                  subPieceCount: Int? = nil,
+                  collectionId: ApolloGQL.BigInt? = nil,
+                  collection: Collection? = nil,
+                  catalogueNumber: Int? = nil,
+                  nickname: String? = nil,
+                  composerId: ApolloGQL.BigInt? = nil,
+                  composer: Composer? = nil,
+                  movements: Movements? = nil
                 ) {
                   self.init(_dataDict: DataDict(
                     data: [
                       "__typename": ApolloGQL.Objects.Piece.typename,
+                      "id": id,
                       "workName": workName,
+                      "totalPracticeTime": totalPracticeTime,
                       "userId": userId,
+                      "lastPracticed": lastPracticed,
+                      "catalogueType": catalogueType,
+                      "keySignature": keySignature,
+                      "format": format,
+                      "instrumentation": instrumentation,
+                      "wikipediaUrl": wikipediaUrl,
+                      "imslpUrl": imslpUrl,
+                      "compositionYear": compositionYear,
+                      "catalogueNumberSecondary": catalogueNumberSecondary,
+                      "catalogueTypeNumDesc": catalogueTypeNumDesc,
+                      "compositionYearDesc": compositionYearDesc,
+                      "compositionYearString": compositionYearString,
+                      "pieceStyle": pieceStyle,
+                      "subPieceType": subPieceType,
+                      "subPieceCount": subPieceCount,
+                      "collectionId": collectionId,
+                      "collection": collection._fieldData,
+                      "catalogueNumber": catalogueNumber,
+                      "nickname": nickname,
+                      "composerId": composerId,
+                      "composer": composer._fieldData,
+                      "movements": movements._fieldData,
                     ],
                     fulfilledFragments: [
-                      ObjectIdentifier(CollectionsQuery.Data.CollectionsCollection.Edge.Node.Pieces.Edge.Node.self)
+                      ObjectIdentifier(CollectionsQuery.Data.CollectionsCollection.Edge.Node.Pieces.Edge.Node.self),
+                      ObjectIdentifier(PieceDetails.self)
                     ]
                   ))
                 }
+
+                public typealias Collection = PieceDetails.Collection
+
+                public typealias Composer = PieceDetails.Composer
+
+                public typealias Movements = PieceDetails.Movements
               }
             }
           }
--- PracticeLogger/Views/CollectionListView.swift
+++ PracticeLogger/Views/CollectionListView.swift
@@ -12,19 +12,29 @@ struct CollectionListView: View {
     @State var collectionId: ApolloGQL.BigInt
     @State private var isLoading: Bool
     @State private var collectionInformation: [CollectionsQuery.Data.CollectionsCollection.Edge]
+    @State private var collectionName: String
+    @State private var hasAppeared = false
+    @State private var selectedPieceForEditing: PieceDetails?
+    @State private var selectedPieceForShow: PieceDetails?
+    var onPieceCreated: ((PieceDetails) -> Void)?
+    var allPieceEdges: [CollectionsQuery.Data.CollectionsCollection.Edge.Node.Pieces.Edge] {
+        collectionInformation.flatMap { $0.node.pieces?.edges ?? [] }
+    }
 
-    init(collectionId: ApolloGQL.BigInt) {
+    init(collectionId: ApolloGQL.BigInt, collectionName: String, onPieceChanged: ((PieceDetails) -> Void)? = nil) {
         self.collectionId = collectionId
+        self.collectionName = collectionName
         self._isLoading = State(initialValue: true)
+        self.onPieceCreated = onPieceChanged
         self._collectionInformation = State(initialValue: [])
     }
 
     // Preview initializer
-    init(preview: [CollectionsQuery.Data.CollectionsCollection.Edge]) {
-        self.collectionId = BigInt(0) // Dummy ID for preview
-        self._isLoading = State(initialValue: false)
-        self._collectionInformation = State(initialValue: preview)
-    }
+//    init(preview: [CollectionsQuery.Data.CollectionsCollection.Edge]) {
+//        self.collectionId = BigInt(0) // Dummy ID for preview
+//        self._isLoading = State(initialValue: false)
+//        self._collectionInformation = State(initialValue: preview)
+//    }
 
     var body: some View {
         VStack {
@@ -37,27 +47,66 @@ struct CollectionListView: View {
             } else if collectionInformation.isEmpty {
                 Text("No pieces found in this collection")
                     .foregroundColor(.secondary)
-            } else {
-                List {
-                    ForEach(collectionInformation[0].node.pieces?.edges ?? [], id: \.self) { item in
-                        HStack {
-                            Text(item.node.workName)
-                            Spacer()
-                            if item.node.userId != nil {
-                                Text("Added")
-                                    .foregroundColor(.green)
-                            } else {
-                                Text("Not added")
-                                    .foregroundColor(.secondary)
+            } else if let edges = collectionInformation.first?.node.pieces?.edges {
+                if edges.isEmpty {
+                    Text("No pieces found in this collection")
+                        .foregroundColor(.secondary)
+                } else {
+                    List {
+                        ForEach(allPieceEdges, id: \.node.id) { item in
+                            VStack(alignment: .leading) {
+                                Text(item.node.workName)
+                                    .font(.headline)
+
+                                if let catalogueType = item.node.catalogueType, let catalogueNumber = item.node.catalogueNumber {
+                                    Text("\(catalogueType.displayName) \(catalogueNumber)")
+                                }
+
+                                if let userId = item.node.userId {
+                                    if let totalPracticeTime = item.node.totalPracticeTime {
+                                        Text(totalPracticeTime.formattedTimeDuration)
+                                            .foregroundColor(.secondary)
+                                    }
+                                } else {
+                                    HStack {
+                                        Image(systemName: "plus.rectangle")
+                                            .foregroundColor(.gray)
+                                    }
+                                }
+                            }
+                            .contentShape(Rectangle())
+                            .onTapGesture {
+                                let tappedPiece = item.node.fragments.pieceDetails
+
+                                if tappedPiece.userId == nil {
+                                    selectedPieceForEditing = tappedPiece
+                                } else {
+                                    selectedPieceForShow = tappedPiece
+                                    onPieceCreated?(tappedPiece)
+                                }
                             }
                         }
                     }
                 }
+            } else {
+                Text("No pieces found in this collection")
+                    .foregroundColor(.secondary)
             }
         }
-        .navigationTitle("Collection")
-        .task {
-            if isLoading {
+        .navigationTitle(collectionName)
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationDestination(item: $selectedPieceForEditing) { piece in
+            PieceEdit(
+                piece: piece,
+                isCreatingNewPiece: true,
+                onPieceCreated: onPieceCreated
+            )
+        }
+
+        .onAppear {
+            guard !hasAppeared else { return }
+            hasAppeared = true
+            Task {
                 await loadCollectionData()
             }
         }
@@ -84,14 +133,13 @@ struct CollectionListView: View {
 
             isLoading = false
         } catch {
-            print("Error loading collection data: \(error)")
             isLoading = false
         }
     }
 }
 
-#Preview {
-    NavigationView {
-        CollectionListView.previewWithRachmaninoff
-    }
-}
+// #Preview {
+//    NavigationView {
+//        CollectionListView.previewWithRachmaninoff
+//    }
+// }
